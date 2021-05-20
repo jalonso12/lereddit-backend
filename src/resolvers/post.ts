7ec60751a7e80bd8db559c8ec522b6b06d41b247
jalonso12 <jalonsoo514@ulacit.ed.cr>
2021-05-20 13:14:00 -0600

    Create | GraphQL EP
    - Built endpoint that creates 1 post to the database

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,19 +1,32 @@
 import { Post } from '../entities/Post';
 import { MyContext } from 'src/types';
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
+  // GETALL
   @Query(() => [Post])
   getPosts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
   }
 
+  //GETONE
   @Query(() => Post, { nullable: true })
   getPost(
     @Arg('id', () => Int) _id: number,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { _id });
+  }
+
+  //CREATE
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title', () => String) title: String,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
   }
 }
