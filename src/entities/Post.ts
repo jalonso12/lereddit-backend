45798a77de6228cb1660ac31ab6ec6f66f3b5169
jalonso12 <jalonsoo514@ulacit.ed.cr>
2021-05-20 12:55:25 -0600

    GetPosts | GraphQL EP
    - Built getPosts endpoint using graphql
    
    - Explicit type checker on post.ts

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,24 @@
 import { Property, PrimaryKey, Entity } from '@mikro-orm/core';
+import { Field, ObjectType } from 'type-graphql';
 
+@ObjectType()
 @Entity()
 export class Post {
+  // @Field exposes the data to graphql
+  // if it is not placed, graphql will not be able to read that specific data
+  @Field()
   @PrimaryKey()
   _id!: number;
 
+  @Field(() => String)
   @Property()
   createdAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
+  @Field()
   @Property({ type: 'text' })
   title!: string;
 }
