3c689479535a5273c94086720631001206af798f
jalonso12 <jalonsoo514@ulacit.ed.cr>
2021-05-20 12:31:25 -0600

    Initial GraphQL Setup
    - Initial setup for using graphql, visit localhost:4000/graphql for the graphql playground

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
+  // DB Connection
   const orm = await MikroORM.init(mikroConfig);
-
   await orm.getMigrator().up();
 
-  // CREATE
-  // const post = orm.em.create(Post, { title: 'My first POST 02' });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  // GET ALL
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('Server running on port:4000...');
+  });
 };
 
 main().catch((err) => {
