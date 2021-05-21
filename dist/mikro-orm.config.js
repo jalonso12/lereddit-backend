3ed227e5744e5eb6815393d2d940bbc1ec1ab6ee
jalonso12 <jalonsoo514@ulacit.ed.cr>
2021-05-21 11:45:01 -0600

    User Entity | GraphQL
    - Created User entity
    
    - Created migration of User entity

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -3,18 +3,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
 const Post_1 = require("./entities/Post");
 const dotenv_1 = __importDefault(require("dotenv"));
 const constants_1 = require("./constants");
+const path_1 = __importDefault(require("path"));
+const User_1 = require("./entities/User");
 dotenv_1.default.config({ path: './config.env' });
 exports.default = {
     migrations: {
         path: path_1.default.join(__dirname, './migrations'),
         pattern: /^[\w-]+\d+\.[tj]s$/,
     },
-    entities: [Post_1.Post],
-    dbName: 'backend-ts-demo',
+    entities: [Post_1.Post, User_1.User],
+    dbName: process.env.DB_NAME,
     password: process.env.DB_PASS,
     type: 'postgresql',
     debug: !constants_1.__prod__,
---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { Property, PrimaryKey, Entity } from '@mikro-orm/core';
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class User {
+  // @Field exposes the data to graphql
+  // if it is not placed, graphql will not be able to read that specific data
+  @Field()
+  @PrimaryKey()
+  _id!: number;
+
+  @Field(() => String)
+  @Property()
+  createdAt: Date = new Date();
+
+  @Field(() => String)
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  @Field()
+  @Property({ type: 'text', unique: true })
+  username!: string;
+
+  @Property({ type: 'text' })
+  password!: string;
+}
